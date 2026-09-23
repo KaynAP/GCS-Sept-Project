81903102d226edadbf2c0fcef23d719c9659f83d
KaynAP <kaubinpinnock1@nait.ca>
2026-09-23 07:35:46 -0600

    Added movement and basic anim

---
+++ GCS Sept Project/Assets/OurScripts/PlayerControl.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float movSpeed;
+    float speedX, speedY;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speedX = Input.GetAxisRaw("Horizontal") * movSpeed;
+        speedY = Input.GetAxisRaw("Vertical") * movSpeed;
+        rb.linearVelocity = new Vector2(speedX, speedY);
+    }
+}
